07f982c164bc96c0ca6914af66844f581cec0e8a
Bashmeet Kaur <bashmeetkaur3@gmail.com>
2026-06-25 23:45:42 +0530

    corrected tranporter

--- controllers/authController.js
+++ controllers/authController.js
@@ -99,24 +99,35 @@ expiresIn: "7d",
 const transporter =
 nodemailer.createTransport({
 
-  host: "smtp.gmail.com",
-
-  port: 587,
-
-  secure: false,
+  service: "gmail",
 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 
-  connectionTimeout: 10000,
-
-  greetingTimeout: 10000,
-
-  socketTimeout: 10000,
-
 });
+
+transporter.verify(
+  function (error, success) {
+
+    if (error) {
+
+      console.log(
+        "SMTP ERROR:",
+        error
+      );
+
+    } else {
+
+      console.log(
+        "SMTP READY"
+      );
+
+    }
+
+  }
+);
 
 // ==============================
 // SIGNUP
